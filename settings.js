2325653a19e5d903d4b98778ed7520beeeacd332
abhixnd <166606042+abhixnd@users.noreply.github.com>
2024-08-17 14:42:13 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,7 +11,7 @@ const FOOTERNAME = '𝐀𝐜𝐡𝐮𝐭𝐭𝐲❤️‍🩹'🎁;
 global.owner = process.env.OWNER_NUMBER  || '919544949116' ;  // ADD YOUR NUMBER WITHOUT +
     
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || ' ' ,
+SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=SFdUwJQB#US6a0Ot3v7Z0LXsFg0gi17mATGrGag2Mefi0TSDt9lM' ,
 travaSend: process.env.TRAVA_SEND || '25' ,   
 MODERATORS : process.env.MODERATORS === undefined ? "919544949116" : process.env.MODERATORS ,    
 MAX_SIZE: process.env.MAX_SIZE === undefined ? '1536': process.env.MAX_SIZE,/*add this in megabytes*/    
